602fb85ae5766fd402dd33ea0c57d55b5ba3dfba
释颜 <xcfox@qq.com>
2021-01-19 09:43:56 +0800

    🐞fix(app): logo path

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import logo from './logo.svg'
+import logo from '../static/logo.svg'
 import './App.css'
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
