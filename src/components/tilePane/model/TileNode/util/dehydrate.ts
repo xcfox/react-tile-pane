d44362f3c7ef723f63bbbcd934646975651e82c5
释颜 <809067559@qq.com>
2021-03-25 01:01:44 +0800

    🐞fix(StretchBar): id is need

--- src/components/tilePane/model/TileNode/util/dehydrate.ts
+++ src/components/tilePane/model/TileNode/util/dehydrate.ts
@@ -7,15 +7,15 @@ import {
 } from '..'
 
 export function leafDehydrate(this: TileLeaf): TileLeafSubstance {
-  const { children, onTab, grow } = this
-  return { children, onTab, grow }
+  const { children, onTab, grow, id } = this
+  return { children, onTab, grow, id }
 }
 
 export function branchDehydrate(this: TileBranch): TileBranchSubstance {
-  const { children, isRow, grow } = this
+  const { children, isRow, grow, id } = this
 
   const childrenDehydrated = children.map((it) =>
     isTileLeaf(it) ? it.dehydrate() : it.dehydrate()
   )
-  return { children: childrenDehydrated, isRow, grow }
+  return { children: childrenDehydrated, isRow, grow, id }
 }
